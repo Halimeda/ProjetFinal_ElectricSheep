25c1a76bd8339bb3a21ca13c182801badc4b1884
Hélène DC <h.decock92@gmail.com>
2019-10-09 16:45:09 +0200

    Start Time add Calculate

---
+++ Electric Sheep/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    DateTime startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = DateTime.UtcNow;
+        Debug.Log(startTime);
+
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSpan calculateTime = -(startTime - DateTime.UtcNow); // Calculate Time Differences between start time and this frame !
+        Debug.Log(calculateTime);
+    }
+}
---
+++ Electric Sheep/Assets/Scripts/epochTimeConvert.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class epochTimeConverter
+{
+    public static long Convert()
+    {
+        TimeSpan calculateTime = DateTime.UtcNow - new DateTime(1970, 1, 1);
+        return((long)calculateTime.TotalSeconds);
+    }
+}
