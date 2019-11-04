6c2bc801a201d8128e9310b9d4cd23334496abeb
Hélène DC <h.decock92@gmail.com>
2019-11-04 15:53:31 +0100

    add sheep and try postpro

--- Electric Sheep/Assets/Scripts/GameManager.cs
+++ Electric Sheep/Assets/Scripts/GameManager.cs
@@ -2,17 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    DateTime startTime;
+    private DateTime startTime;
+    private DateTime lastConnexion;
+    private long temp;
+
+    TimeSpan lastCoDiff;
+
+    private void Awake()
+    {
+        startTime = DateTime.UtcNow;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        startTime = DateTime.UtcNow;
-        Debug.Log(startTime);
-
         
+        Debug.Log(startTime + "Strart Time");
+        Debug.Log(PlayerPrefs.GetString("lastCo") + "Player prefs lastco");
+        PlayerPrefs.SetString("lastCo", System.DateTime.Now.ToBinary().ToString());
+
+        lastCoDiff = this.FromLastConnexion(startTime);
+        Debug.Log(lastCoDiff + "Timespan last co");
     }
 
 
@@ -20,6 +34,27 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         TimeSpan calculateTime = -(startTime - DateTime.UtcNow); // Calculate Time Differences between start time and this frame !
-        Debug.Log(calculateTime);
+        Debug.Log(calculateTime + "Calculate time");
+        ManagePlayerData.AutoSave();
+        Debug.Log(PlayerPrefs.GetString("lastCo") + "Player prefs lastco");
+
+
     }
+
+    public TimeSpan FromLastConnexion(DateTime startTime)
+    {
+
+        temp = Convert.ToInt64(PlayerPrefs.GetString("lastCo"));
+
+        //Convert the old time from binary to a DataTime variable
+        lastConnexion = DateTime.FromBinary(temp);
+        Debug.Log("oldDate: " + lastConnexion);
+
+        //Use the Subtract method and store the result as a timespan variable
+        TimeSpan difference = startTime.Subtract(lastConnexion);
+        Debug.Log("Difference: " + difference);
+        return difference;
+    }
+
+
 }
---
+++ Electric Sheep/Assets/Scripts/ManagePlayerData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ManagePlayerData
+{
+
+
+
+    public static void AutoSave() //Add Sheep Stats
+    {
+
+        PlayerPrefs.SetString("lastCo", System.DateTime.UtcNow.ToBinary().ToString());
+        
+        Debug.Log("Saving this date to prefs: " + System.DateTime.UtcNow);
+    }
+
+    public static void QuitApp()
+    {
+        //Savee the current system time as a string in the player prefs class
+        PlayerPrefs.SetString("lastCo", System.DateTime.UtcNow.ToBinary().ToString());
+
+        Debug.Log("Saving this date to prefs: " + System.DateTime.UtcNow);
+
+        Application.Quit();
+    }
+}
