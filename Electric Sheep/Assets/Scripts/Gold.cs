46dddd75f6c6b4a7f908edb7823099b690eeace1
Hélène DC <h.decock92@gmail.com>
2019-12-05 13:54:44 +0100

    truc

--- Electric Sheep/Assets/Scripts/Gold.cs
+++ Electric Sheep/Assets/Scripts/Gold.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gold : MonoBehaviour
 {
     public static int playerGold = 0;
+    public Text text;
+
+    public void Start()
+    {
+        text.text = "Gold : " + playerGold;
+    }
 }
