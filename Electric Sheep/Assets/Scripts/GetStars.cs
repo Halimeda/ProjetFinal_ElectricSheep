5419e03da6aaea5e8d3496e35564fb38d560dd7e
Hélène DC <h.decock92@gmail.com>
2019-12-15 19:58:45 +0100

    add new scene

---
+++ Electric Sheep/Assets/Scripts/GetStars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetStars : MonoBehaviour
+{
+    public int score = 0;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "star")
+        {
+            score++;
+            Destroy(collision.gameObject);
+            Debug.Log(score);
+            Debug.Log("Star");
+        }
+    }
+}
