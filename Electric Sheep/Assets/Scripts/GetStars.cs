07b294405f7ad9991c8e9163a482c05b3c755900
Hélène DC <h.decock92@gmail.com>
2019-12-16 16:08:10 +0100

    run scene

--- Electric Sheep/Assets/Scripts/GetStars.cs
+++ Electric Sheep/Assets/Scripts/GetStars.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class GetStars : MonoBehaviour
 {
+    public GameObject[] gameObjects;
     public int score = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "star")
+        if (collision.gameObject.tag == "Bonus")
         {
             score++;
-            Destroy(collision.gameObject);
+            collision.transform.SetPositionAndRotation(new Vector3 (collision.transform.position.x, 350, 0), Quaternion.identity) ;
             Debug.Log(score);
-            Debug.Log("Star");
+            Debug.Log("Bonus");
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Miss")
+        {
+            Destroy(this.gameObject);
+            Debug.Log("You loose");
         }
     }
+
+    private void CreateBonusMiss()
+    {
+        
+    }
+
 }
